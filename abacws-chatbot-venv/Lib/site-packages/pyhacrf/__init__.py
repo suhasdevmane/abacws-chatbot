bcb516a1d5f7d83e98caa07ab4d16c02903eaef2
suhas devmane <suhasdevmane@outlook.com>
2023-10-25 16:06:20 +0100

    Delete abacws-chatbot-venv directory

--- abacws-chatbot-venv/Lib/site-packages/pyhacrf/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Implements a Hidden Alignment Conditional Random Field (HACRF). """
-
-from .pyhacrf import Hacrf
-from .feature_extraction import StringPairFeatureExtractor, PairFeatureExtractor
-
-__all__ = ['Hacrf', 'StringPairFeatureExtractor', 'PairFeatureExtractor']
